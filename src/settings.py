0e10cc0f5173f0811db74251941cca7a28443c82
Богдан Богдан <rezniuk@gmail.com>
2023-10-01 08:35:22 -0700

    give env to load_dotenv

--- src/settings.py
+++ src/settings.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(".env.template")
 
 GHOST_SPEED = float(os.getenv("GHOST_SPEED"))
 ATTACK_RADIUS = float(os.getenv("ATTACK_RADIUS"))
